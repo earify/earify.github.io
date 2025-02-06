e21518a0667c434af7e6e7f6b191680fe66e08a9
earify <112871717+earify@users.noreply.github.com>
2025-02-06 09:26:49 +0900

    app.js 업데이트

--- notepad/app.js
+++ notepad/app.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDgoWPHwKnm0TNtZVAvd9W3Vgzk4T5MRVY",
   authDomain: "temp-notepad.firebaseapp.com",
   projectId: "temp-notepad",
+projectId: "temp-notepad",
   storageBucket: "temp-notepad.firebasestorage.app",
   messagingSenderId: "597209729146",
   appId: "1:597209729146:web:a9d13f6eb5f093148d93c3",
